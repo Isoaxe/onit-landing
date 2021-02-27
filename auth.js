a94b98464561d954cf1330e1a464ef1baf585c29
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-02-27 14:09:17 +0700

    Create auth.js
    Configure and initialise Firebase in a JavaScript file rather than via HTML scripts.

---
+++ auth.js
@@ -0,0 +1,16 @@
+// Firebase app always required to be imported and listed first.
+const firebase = require('firebase/app');
+require('firebase/auth');
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyD7OtnYqeu-9sEiEJKl1bkGpaRBEzTFfy0',
+  authDomain: 'onit-aaa6e.firebaseapp.com',
+  projectId: 'onit-aaa6e',
+  storageBucket: 'onit-aaa6e.appspot.com',
+  messagingSenderId: '451988465101',
+  appId: '1:451988465101:web:335beb05b535b1257d9084',
+  measurementId: 'G-CB3S63219R'
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
