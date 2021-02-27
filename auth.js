1e08a70e3d4d40399c1c2e74bca12c970d0e3e5d
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-02-27 15:03:02 +0700

    Auth: Specify signin options
    Allow email and Google sign in. Facebook and phone can be added at a later date.

--- auth.js
+++ auth.js
@@ -18,3 +18,11 @@ firebase.initializeApp(firebaseConfig);
 
 // Initialize the FirebaseUI Widget using Firebase.
 const ui = new firebaseui.auth.AuthUI(auth);
+
+ui.start('#firebaseui-auth-container', {
+  signInOptions: [
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID
+  ]
+  // Other config options...
+});
